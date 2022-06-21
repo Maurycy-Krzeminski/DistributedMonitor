3de922b5194312488d22b333c75d4e9dcee54024
maurycy <maurycy.krzeminski@gmail.com>
2022-06-21 22:29:09 +0200

    Instead of Lock and condition use ArrayBlockingQueue<Token> with Capacity 1

--- src/main/kotlin/org/maurycy/monitor/required/Monitor.kt
+++ src/main/kotlin/org/maurycy/monitor/required/Monitor.kt
@@ -6,8 +6,7 @@ import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
-import java.util.concurrent.locks.ReentrantLock
-import kotlin.concurrent.withLock
+import java.util.concurrent.ArrayBlockingQueue
 
 class Monitor<T : State>(
     aConstructor: () -> T,
@@ -18,18 +17,17 @@ class Monitor<T : State>(
     private val numberOfProcesses = aPeers.size
     private val rn: MutableList<Int> = MutableList(numberOfProcesses) { 0 }
     private val format = Json { classDiscriminator = "#class" }
-    private val token: MutableList<Token?> = MutableList(1) { null }
 
     private var state: T = aConstructor()
 
-    private val lock = ReentrantLock()
-    private val condition = lock.newCondition()
+    private val blockingQueue = ArrayBlockingQueue<Token>(1)
+
     private val zmqCommunicator = ZmqCommunicator(address = aPeers[aId], peers = aPeers)
     private val processMessages = ProcessMessages()
 
     init {
         if (this.aId == 0) {
-            token[0] = Token(mutableListOf(), MutableList(numberOfProcesses) { 0 })
+            blockingQueue.add(Token(mutableListOf(), MutableList(numberOfProcesses) { 0 }))
         }
 
         CoroutineScope(Dispatchers.IO).launch {
@@ -45,10 +43,10 @@ class Monitor<T : State>(
                 if (message.id == 0) {
                     processMessages.processRequestMessage(message, rn)
                 } else if (message.id.minus(1) == this@Monitor.aId) {
-                    lock.withLock {
-                        processMessages.processTokenMessage(message, token, state)
-                        condition.signalAll()
-                    }
+//                    lock.withLock {
+                    processMessages.processTokenMessage(message, blockingQueue, state)
+//                        condition.signal()
+//                    }
                 }
             }
         }
@@ -57,46 +55,51 @@ class Monitor<T : State>(
     fun run(aCanTaskBeExecuted: T.() -> Boolean = { true }, aTask: T.() -> Unit) {
         var executed = false
         while (!executed) {
-            if (token[0] == null) {
+            if (blockingQueue.size == 0) {
                 Thread.sleep(1000)
                 rn[this.aId]++
                 zmqCommunicator.sendRequestMessage(this.aId, this.rn[this.aId])
             }
-            lock.withLock {
-                while (token[0] == null) {
-                    condition.await()
-                }
+            val token = blockingQueue.take()
 
-                if (state.aCanTaskBeExecuted()) {
-                    state.apply(aTask)
-                    executed = true
-                }
-                updateQueueAndTryToSendToken()
+//            lock.withLock {
+//                while (token[0] == null) {
+//                    condition.await()
+//                }
+
+            if (state.aCanTaskBeExecuted()) {
+                state.apply(aTask)
+                executed = true
             }
+            blockingQueue.put(token)
+            updateQueueAndTryToSendToken()
+//            }
+
         }
     }
 
     private fun updateQueueAndTryToSendToken() {
-        token[0]!!.LN[this.aId] = rn[this.aId]
-
+        val token = blockingQueue.take()
+        token.LN[this.aId] = rn[this.aId]
         for (i in 0 until numberOfProcesses) {
-            val isIndexInQueue = token[0]!!.queue.contains(i + 1)
-            val isWaitingForCriticalSection = token[0]!!.LN[i] < rn[i]
+            val isIndexInQueue = token.queue.contains(i + 1)
+            val isWaitingForCriticalSection = token.LN[i] < rn[i]
             if (!isIndexInQueue && isWaitingForCriticalSection) {
-                token[0]!!.queue.add(i + 1)
+                token.queue.add(i + 1)
             }
         }
 
-        val processNumber = token[0]!!.queue.removeFirstOrNull()
+        val processNumber = token.queue.removeFirstOrNull()
         if (processNumber != null) {
-            zmqCommunicator.sendTokenMessage(aProcessNumber = processNumber, aToken = token[0], aState = state)
-            token[0] = null
+            zmqCommunicator.sendTokenMessage(aProcessNumber = processNumber, aToken = token, aState = state)
+        } else {
+            blockingQueue.put(token)
         }
     }
 
     fun fin() {
         var remainingTimeout = aFinishTimeout
-        while (token[0] != null && remainingTimeout > 0) {
+        while (blockingQueue.element() != null && remainingTimeout > 0) {
             updateQueueAndTryToSendToken()
             Thread.sleep(50)
             remainingTimeout -= 50
--- src/main/kotlin/org/maurycy/monitor/required/ProcessMessages.kt
+++ src/main/kotlin/org/maurycy/monitor/required/ProcessMessages.kt
@@ -1,5 +1,7 @@
 package org.maurycy.monitor.required
 
+import java.util.concurrent.ArrayBlockingQueue
+
 class ProcessMessages {
     fun processRequestMessage(aMessage: Message, aRN: MutableList<Int>) {
         val senderNumber = aMessage.senderNumber!!
@@ -7,10 +9,13 @@ class ProcessMessages {
         aRN[senderNumber - 1] = maxOf(aRN[senderNumber - 1], requestNumber)
     }
 
-    fun processTokenMessage(aMessage: Message, aToken: MutableList<Token?>, aState: State) {
+    fun processTokenMessage(aMessage: Message, aToken: ArrayBlockingQueue<Token>, aState: State) {
         aMessage.state?.let {
             aState.deserialize(it)
-            aMessage.token?.let { tok -> aToken.set(0, tok) }
+            aMessage.token?.let { tok ->
+                aToken.clear()
+                aToken.add(tok)
+            }
 
         }
     }
